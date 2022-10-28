d1293f850e87ee46f4189cabd3ed6ac328b4c177
akiasa oshima <asaolr4@gmail.com>
2022-10-28 14:34:10 +0900

    一覧画面機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,20 @@
 class TasksController < ApplicationController
   def index
-
+    @tasks = Task.all
   end
 
   def new
-
+    @task = Task.new
   end
 
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
   end
 
   def show
@@ -21,5 +27,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
