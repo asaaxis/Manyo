71aabc051645c9dc627dacaaabaaa8215a8ea7cf
akiasa oshima <asaolr4@gmail.com>
2022-10-30 00:27:43 +0900

    System Specのテスト作成

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :task do
+    title { 'Factoryで作ったデフォルトのタイトル1' }
+    content { 'Factoryで作ったデフォルトのコンテント1' }
+  end
+  factory :second_task, class: Task do
+    title { 'Factoryで作ったデフォルトのタイトル2' }
+    content { 'Factoryで作ったデフォルトのコンテント2' }
+  end
+end
