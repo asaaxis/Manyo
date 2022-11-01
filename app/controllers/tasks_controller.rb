5799ec6b32ab912301e4472882176763ca8589ff
akiasa oshima <asaolr4@gmail.com>
2022-11-01 13:24:45 +0900

    タスクにステータス登録機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,6 +48,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content, :limit)
+    params.require(:task).permit(:title, :content, :limit, :status)
   end
 end
