3f670e4bfaae803b97e08b90ed6d5aae1c60e564
akiasa oshima <asaolr4@gmail.com>
2022-11-01 13:06:12 +0900

    statusカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_020058) do
+ActiveRecord::Schema.define(version: 2022_11_01_040355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_10_31_020058) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "limit", default: -> { "CURRENT_DATE" }, null: false
+    t.string "status"
   end
 
 end
