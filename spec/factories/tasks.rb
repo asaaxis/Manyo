a035b4ab3ec81438a78f4225f8f70891f5cb1e27
akiasa oshima <asaolr4@gmail.com>
2022-11-02 11:47:54 +0900

    タスク新規登録でステータス登録, 検索のテスト機能実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,10 +3,12 @@ FactoryBot.define do
     title { 'Factoryで作ったデフォルトのタイトル1' }
     content { 'Factoryで作ったデフォルトのコンテント1' }
     limit { 'Date.time' }
+    status { '未着' }
   end
   factory :second_task, class: Task do
     title { 'Factoryで作ったデフォルトのタイトル2' }
     content { 'Factoryで作ったデフォルトのコンテント2' }
     limit { 'Date.time' }
+    status { '未着' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task) { FactoryBot.create(:task, title: 'task', content: 'tasks') }
+  let!(:task) { FactoryBot.create(:task, title: 'task', content: 'tasks', limit: '2022/11/03', status: '完了') }
     FactoryBot.create(:second_task, title: '付け加えた名前', content: '付け加えたコンテント')
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in "タイトル", with: 'task'
-        fill_in "内容", with: 'tasks'
+        fill_in 'タイトル', with: 'task'
+        fill_in '内容', with: 'task'
         click_on '登録'
         expect(page).to have_content 'task'
         expect(page).to have_content 'tasks'
+        expect(page).to have_content '2022-11-03'
+        expect(page).to have_content '完了'
       end
     end
   end
@@ -32,8 +34,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context '終了期限でソートするボタンを押した場合' do
       it '終了期限が遅いタスクが一番上に表示される' do
-        task = FactoryBot.create(:task, limit: '2022-11-03')
-        click_on "終了期限でソートする"
+        visit tasks_path
+        FactoryBot.create(:task, limit: '2022-11-03')
+        click_on '終了期限でソートする'
         task_list = all('tbody')
         expect(task_list[0]).to have_content '2022-11-03'
       end
@@ -43,6 +46,39 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
         visit task_path(task.id)
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+  describe '検索機能' do
+    before do
+      FactoryBot.create(:task, title: "task")
+      FactoryBot.create(:second_task, title: "sample")
+    end
+
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        visit tasks_path
+        fill_in 'task_title', with: 'task'
+        click_on 'search'
+        expect(page).to have_content 'task'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        visit tasks_path
+        select '完了', from: 'task[status]'
+        click_on 'search'
+        expect(page).to have_content '完了'
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        visit tasks_path
+        select '完了', from: 'task[status]'
+        fill_in 'task_title', with: 'task'
+        click_on 'search'
+        expect(page).to have_content '完了'
         expect(page).to have_content 'task'
       end
     end
