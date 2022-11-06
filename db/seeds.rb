40d37d2d5a06c91577db98ff3ed9a06240f83b8d
akiasa oshima <asaolr4@gmail.com>
2022-11-06 22:00:39 +0900

    管理者画面の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-50.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(name: name,
-                email: email,
-                password_digest: password,
-                )
-end
+User.create!(name: "管理者",
+              email: "admin@example.com",
+              password: "333333",
+              password_confirmation: "333333",
+              admin: true)
+
+# 50.times do |n|
+#   name = Faker::Games::Pokemon.name
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(name: name,
+#                 email: email,
+#                 password_digest: password,
+#                 )
+# end
