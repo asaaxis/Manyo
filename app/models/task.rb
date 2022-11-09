b293a38e7cb98c69dfbca8ee29c9c5df5062f8a9
akiasa oshima <asaolr4@gmail.com>
2022-11-09 22:11:09 +0900

    ラベル機能実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,11 +3,14 @@ class Task < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
-  
+  has_many :labelings, dependent: :destroy
+  has_many :labels, through: :labelings
+
   enum status: {未着: 0, 着手中: 1, 完了: 2}
   enum priority: {高: 0, 中: 1, 低: 2}
 
   scope :status, -> (status){where(status: status)}
   scope :title, -> (title){where("title LIKE ?", "%#{title}%")}
   scope :title_status, -> (title,status){where("title LIKE ?", "%#{title}%").where(status: status)}
+  # scope :label, -> (label){where(label: label)}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
-User.create!(name: "管理者",
-              email: "admin@example.com",
-              password: "333333",
-              password_confirmation: "333333",
-              admin: true)
+# User.create!(name: "管理者",
+#               email: "admin@example.com",
+#               password: "333333",
+#               password_confirmation: "333333",
+#               admin: true)
 
 
-User.create!(name: "taro",
-                email: "taro@example.com",
-                password: "111111",
-                password_confirmation: "111111",
-                )
+# User.create!(name: "taro",
+#                 email: "taro@example.com",
+#                 password: "111111",
+#                 password_confirmation: "111111",
+#                 )
+
+5.times do |i|
+  Label.create!(name: "sample#{i + 1}")
+end
