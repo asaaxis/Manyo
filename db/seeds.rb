b8c6959857d5ac139f51c290cf6fc0044c450a5d
akiasa oshima <asaolr4@gmail.com>
2022-11-10 14:37:42 +0900

    シードデータ10件分作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
-# User.create!(name: "管理者",
-#               email: "admin@example.com",
-#               password: "333333",
-#               password_confirmation: "333333",
-#               admin: true)
+User.create!(name: "管理者",
+              email: "admin@example.com",
+              password: "333333",
+              password_confirmation: "333333",
+              admin: true)
 
+10.times do |i|
+  User.create!(name: "taro#{i + 1}",
+              email: "taro#{i + 1}@example.com",
+              password: "11111#{i + 1}",
+              password_confirmation: "11111#{i + 1}"
+                  )
+end
 
-# User.create!(name: "taro",
-#                 email: "taro@example.com",
-#                 password: "111111",
-#                 password_confirmation: "111111",
-#                 )
+10.times do |i|
+  Task.create!(title: "ara#{i + 1}",
+              content: "asa#{i + 1}",
+              user_id: rand(1..10)
+              )
+end
 
-5.times do |i|
+10.times do |i|
   Label.create!(name: "sample#{i + 1}")
 end
